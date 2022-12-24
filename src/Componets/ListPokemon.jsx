323fab564de9894e156d1ad3f16ea8c40ef1e790
Loida Gonzalez <ljgsnh14@gmail.com>
2022-12-24 18:52:00 -0500

    update

--- src/Componets/ListPokemon.jsx
+++ src/Componets/ListPokemon.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PokemonCard from './PokemonCard'
-import"./styles/listPokemon.css"
+import '../Componets/styles/ListPokemon.css'
 
 const ListPokemon = ({pokemons}) => {
   
